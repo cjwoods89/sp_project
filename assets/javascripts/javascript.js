bb4ba673fbfab6338143b9c0e0815fc6ad46f964
Christopher Woods <Christopher.Woods@LibertyMutual.com>
2017-06-12 15:44:58 -0400

    Modify HTML and Add PUT AJAX request

--- assets/javascripts/javascript.js
+++ assets/javascripts/javascript.js
@@ -4,12 +4,13 @@
   var introButton = $('#intro');
   var getButton = $('#get');
   var postButton = $('#post');
-  var putButton = $('#put');
+  var putButton = $('.glyphicon-floppy-disk');
   var deleteButton = $('.glyphicon-remove');
   var addButton = $('.glyphicon-add');
   var addTable = $('.addTable');
   var resetTable = $('.resetTable');
   var allSeasons = $('#allSeasons');
+  var testTable = $('#getTable td');
 
   // Areas of the DOM
   var introContent = $('#introContent');
@@ -20,11 +21,12 @@
   var getTable = $('#getTable');
   var postTable = $('#postTable');
   var alert = $('.alert_container');
+  var facts= $('#facts');
 
   var getPagination = $('#getPagination')
 
   // Hiding for now, as I'm unsure what I want to put in the container
-  $('.apiNavigation').hide();
+  // $('.apiNavigation').hide();
 
   // Used to reset the "active" on the anchor tags in the introContent
   function resetAttr(button) {
@@ -53,6 +55,28 @@
 
   }
 
+  function getRandomInt(min, max) {
+      return Math.floor(Math.random() * (max - min + 1)) + min
+  }
+
+  function getFacts(){
+
+    facts.empty();
+    var randomID = getRandomInt(1,20);
+
+    var getFacts = $.get( "http://localhost:1337/fact/" + randomID , function( data ) {
+
+      getFacts.done(function(data){
+
+        facts.append(
+          '<h4>DID YOU KNOW?<small><p><br/>' + data.fact + '</p></small></h4>')
+
+      });
+
+    }); // Close getFacts get api
+
+  }; // Close getFacts function
+
   function getData(){
 
     resetContent($(this));
@@ -68,7 +92,9 @@
 
           getTable.append(
             '<tr> <td> <span class="glyphicon glyphicon-remove delete" aria-hidden="true" data-episodeID="' + data[i].id +
-            '"></span> </td>' +
+            '"> </span>  ' +
+            '<span class="glyphicon glyphicon-floppy-disk put" aria-hidden="true" data-episodeIDTest="' + data[i].id +
+            '"> </span> </td>' +
             '<td class="season">' + data[i].season + '</td>' +
             '<td class="episode">' + data[i].ep_num + '</td>' +
             '<td class="title">' + data[i].title + '</td>' +
@@ -99,13 +125,14 @@
 
   introButton.click(function(){
 
+    getFacts();
     resetAttr($(this));
     resetHide(introContent);
 
   });
 
   getButton.click(function(){
-    // debugger;
+    getFacts();
     resetAttr($(this));
     resetHide(getContent);
 
@@ -113,10 +140,12 @@
   });
 
   getPagination.on("click","#allSeasons",function(){
+    getFacts();
     getData();
   })
 
   getPagination.on("click",".seasonButton",function(){
+    getFacts();
 
     var seasonButton = $(this).text();
     $("#getTable td").remove();
@@ -133,11 +162,13 @@
 
           getTable.append(
             '<tr> <td> <span class="glyphicon glyphicon-remove delete" aria-hidden="true" data-episodeID="' + data[i].id +
-            '"></span> </td>' +
+            '"> </span>  ' +
+            '<span class="glyphicon glyphicon-floppy-disk put" aria-hidden="true" data-episodeIDTest="' + data[i].id +
+            '"> </span> </td>' +
             '<td class="season">' + data[i].season + '</td>' +
             '<td class="episode">' + data[i].ep_num + '</td>' +
             '<td class="title">' + data[i].title + '</td>' +
-            '<td class="description">' + data[i].description + '</td>' +
+            '<td class="description">' + data[i].description + '</span></td>' +
             '<td class="date">' + goodDate.substring(0, goodDate.indexOf('T')) + '</td> </tr>'
           )
         };
@@ -146,6 +177,7 @@
 });
 
   postButton.click(function(){
+    getFacts();
 
     resetAttr($(this));
     resetHide(postContent);
@@ -153,6 +185,7 @@
   });
 
   addTable.click(function(){
+    getFacts();
 
     postTable.append(
       '<tr>' +
@@ -168,10 +201,12 @@
   });
 
   resetTable.click(function(){
+    getFacts();
     $("#postTable td").remove();
   });
 
   postTable.on("click", ".plus", function(){
+    getFacts();
 
     var seasonData = $(this).closest('tr').find(".postSeason").val();    // Find the input
     var episodeData = $(this).closest('tr').find(".postEpisode").val();    // Find the input
@@ -205,16 +240,38 @@
 
   });
 
+  // What to do when the PUT button is pressed
+  getTable.on("click", ".put", function(){
+    getFacts();
 
-  putButton.click(function(){
+    var record_ID = $(this).attr('data-episodeIDTest');
 
-    resetAttr($(this));
-    resetHide(putContent);
+    var seasonData = $(this).closest('tr').find(".season").text();    // Find the input
+    var episodeData = $(this).closest('tr').find(".episode").text();    // Find the input
+    var titleData = $(this).closest('tr').find(".title").text();    // Find the input
+    var descriptionData = $(this).closest('tr').find(".description").text();    // Find the input
+    var air_dateData = $(this).closest('tr').find(".air_date").text();    // Find the input
+
+    var dataObject = {"season":seasonData,"ep_num":episodeData,"title":titleData,"description":descriptionData,"orig_air_date":air_dateData}
+
+    $.ajax({
+      url: "http://localhost:1337/episode/" + record_ID,
+      type: "PUT",
+      data: dataObject,
+      success:  function (){
+        getData();
+      },
+      error: function( jqXhr, textStatus, errorThrown ){
+          console.log( errorThrown );
+      }
+
+    });
 
   });
 
   // What to do when the delete button is pressed
   getTable.on("click", ".delete", function(){
+    getFacts();
 
     var record_ID = $(this).attr('data-episodeID');
 
@@ -231,5 +288,14 @@
     });
 
   });
+
+  // What to do when the user clicks inside the table content (allows for PUT to work)
+  getTable.on("mousedown", $('#getTable td'), function(event) {
+
+    $(event.target).closest('td').prop("contentEditable", true);
+
+  });
+
+getFacts();
 
 })();
